3507875933df5467f8aba50aab2cde37ae48751e
Onifade Joshua <onifadjosh@gmail.com>
2024-01-17 06:00:58 +0100

    Added marquee for better experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import Community from "./components/Community";
 import Footer from "./components/Footer";
+import Marquee from "./components/Marquee";
 function App() {
   
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Section/>
       <Community></Community>
+      <Marquee></Marquee>      
       <Footer></Footer>
     </>
   )
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,22 @@
+
+
+const Marquee = () => {
+  return (
+    <div>
+        <div className="my-10">
+          <a href="mailto:" className="overflow-hidden flex gap-10 text-[100px] font-bold whitespace-nowrap tracking-[1px] uppercase marquee">
+            <p>I bring creativity and expertise to your projects.
+            Let&apos;s elevate your digital presence together!
+            Hit me up &#128584;, let&apos;s collaborate.</p>
+
+            <p>I bring creativity and expertise to your projects.
+            Let&apos;s elevate your digital presence together!
+            Hit me up &#128584;, let&apos;s collaborate.</p>
+          </a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Marquee
