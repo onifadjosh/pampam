1b877fa46ceefabaec6c20acd3499790dd734fb5
Onifade Joshua <onifadjosh@gmail.com>
2024-01-20 12:50:20 +0100

    Styling changes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 const Section = () => {
 
     useGSAP(()=>{
-        gsap.to("#smooth-wrapper", {
+        gsap.to(".project1", {
             x: '40%',
             ease: "slow(0.9,0.1,false)",
             // start:"+600",
@@ -126,68 +126,64 @@ const Section = () => {
 
   return (
     <div className='projects overflow-hidden px-10'>
-        <div className='aesthetics ' id="aesth">
-            <span className="flex justify-end relative">
+        <div className='aesthetics relative' id="aesth">
+            <span className="flex justify-end relative font-Mont">
                 <h3 className="absolute left-[30%]">SELECTED PROJECTS</h3>
 
-                <h3>Aesthetics/
+                <h3 className="">Aesthetics/
                 &ldquo;Greensock&ldquo;
                 </h3>
             </span>
 
-           <div id="smooth-wrapper">
-                <div id="smooth-content">
-                    <h1 data-letters="HUMANKIND-CLONE WITH GSAP" className='site project1 relative left-0 //hover:text-[#133020] //hover:text-[#def54f]'>HUMANKIND-CLONE WITH GSAP</h1>
-                </div>
-           </div>
+            <h1 data-letters="HUMANKIND-CLONE WITH GSAP" className='site project1 absolute left-0 font-Noto'>HUMANKIND-CLONE WITH GSAP</h1>
         </div>
         
         <span className="flex justify-center  w-full h-[2px] bg-[#545454] border-line1"></span>
 
-        <div className='harmony'>
-            <span>
+        <div className='harmony relative'>
+            <span className="font-Mont">
                 <h3>Harmony/ 
                 &quot;Vanillas&quot;
                 </h3>
             </span>
 
-            <h1 data-letters="CRYPTO WEBPAGE WITH VANILLAS" className='site project2 relative left-[60%]'> CRYPTO WEBPAGE WITH VANILLAS </h1>
+            <h1 data-letters="CRYPTO WEBPAGE WITH VANILLAS" className='site project2 absolute left-[60%] font-Noto'> CRYPTO WEBPAGE WITH VANILLAS </h1>
         </div>
 
         <span className="flex justify-center  w-full h-[2px] bg-[#545454] border-line2"></span>
 
-        <div className='aesthetics'>
-            <span>
+        <div className='aesthetics relative'>
+            <span className="font-Mont">
                 <h3>Aesthetics/ 
                 &quot;Vanillas&quot;
                 </h3>
             </span>
 
-            <h1 data-letters="REBOUND-A WINE BOTTLE STORE CLONE" className='site project3 relative left-[-50%]'>REBOUND-A WINE BOTTLE STORE CLONE</h1>
+            <h1 data-letters="REBOUND-A WINE BOTTLE STORE CLONE" className='site project3 absolute left-[-50%] font-Noto'>REBOUND-A WINE BOTTLE STORE CLONE</h1>
         </div>
 
         <span className="flex justify-center  w-full h-[2px] bg-[#545454] border-line3"></span>
 
-        <div className='harmony bolt'> 
-            <span>
+        <div className='harmony bolt relative'> 
+            <span className="font-Mont">
                 <h3>Harmony/ 
                 &ldquo;Greensock&ldquo;
                 </h3>
             </span>
 
-            <h1 data-letters="BOLT - SHOPIFY MARKETPLACE" className='site project4 relative left-[50%]'>BOLT - SHOPIFY MARKETPLACE</h1>
+            <h1 data-letters="BOLT - SHOPIFY MARKETPLACE" className='site project4 absolute left-[50%] font-Noto'>BOLT - SHOPIFY MARKETPLACE</h1>
         </div>
 
         <span className="flex justify-center  w-full h-[2px] bg-[#545454] border-line4"></span>
 
-        <div className='harmony tech-time'>
-            <span>
+        <div className='harmony tech-time relative'>
+            <span className="font-Mont">
                 <h3>Harmony/ 
                 &quot;Vanillas&quot;
                 </h3>
             </span>
 
-            <h1 data-letters="TECHTIME - LEARN TECH ONLINE" className='site project5 relative left-[-50%]'>TECHTIME - LEARN TECH ONLINE</h1>
+            <h1 data-letters="TECHTIME - LEARN TECH ONLINE" className='site project5 absolute left-[-50%] font-Noto'>TECHTIME - LEARN TECH ONLINE</h1>
         </div>
 
         <span className="flex justify-center  w-full h-[2px] bg-[#545454] border-line5"></span>
