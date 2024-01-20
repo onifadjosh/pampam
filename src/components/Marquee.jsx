0f1b6d51653b8e6751a707bbdb48ad0c5b1bd2a2
Onifade Joshua <onifadjosh@gmail.com>
2024-01-20 12:49:56 +0100

    added fonts for UI

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,8 +3,8 @@
 const Marquee = () => {
   return (
     <div>
-        <div className="my-10">
-          <a href="mailto:onifadjosh@gmail.com" className="overflow-hidden flex gap-10 text-[100px] font-bold whitespace-nowrap tracking-[1px] uppercase marquee">
+        <div className="my-10 font-Noto">
+          <a href="mailto:onifadjosh@gmail.com" className="overflow-hidden flex gap-10 lg:text-[75px] xl:text-[100px] font-bold whitespace-nowrap tracking-[1px] uppercase marquee">
             <p>I bring creativity and expertise to your projects.
             Let&apos;s elevate your digital presence together!
             Hit me up &#128584;, let&apos;s collaborate.</p>
