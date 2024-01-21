82b2e3a69d8b09a053dd17de6e74792493be8c4a
Onifade Joshua <onifadjosh@gmail.com>
2024-01-21 22:31:35 +0100

    Styling changes and responsivenss for medium/tablet screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@
 
 const Footer = () => {
     return (
-      <div className="relative h-[50vh] overflow-hidden pb-5  ">
+      <div className="relative h-[40vh] lg:h-[50vh] overflow-hidden pb-5  ">
         
         
 
         <div className="flex flex-col justify-between h-full  px-10 ">
           
           <div className="flex relative flex-col justify-center items-center z-10">
-            <p className="text-[50px] text-center pb-10 font-Noto">Looking for a Frontend Developer?</p>
+            <p className="text-[35px] lg:text-[50px] text-center pb-10 font-Noto">Looking for a Frontend Developer?</p>
     
             <a href="mailto:onifadjosh@gmail.com" className="button text-[18px] z-[10] px-5 py-3 rounded-[30px] border-white border-[1px] font-bold cursor-pointer ">
               <p className="button-content font-Mont">GET IN TOUCH</p>
@@ -18,7 +18,7 @@ const Footer = () => {
           </div>
     
           <div className="absolute lg:top-[10%] xl:top-[-5%] left-[5%] opacity-[10%]">
-            <h1 className="text-white lg:text-[220px] xl:text-[300px] font-bold font-Noto">HIRE ME</h1>
+            <h1 className="text-white md:text-[170px] lg:text-[220px] xl:text-[300px] font-bold font-Noto">HIRE ME</h1>
           </div>
     
           <div className="flex justify-between font-bold">
